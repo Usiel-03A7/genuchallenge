1fd0f3db2705f6045bb7fa6ddfd43b8ad9f6b83b
Usiel-03A7 <shebastyan@gmail.com>
2025-04-21 15:40:40 -0600

    Remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ import GlobalPresence from "./components/GlobalPresence";
 function App() {
   return (
     <div className="min-h-screen bg-white">
-      {/* Sección Hero (Imagen 1) */}
+
       <div className="flex">
         <HeroLeft />
         <Sidebar />
       </div>
 
-      {/* Sección Misión (Imagen 2) */}
+
       <MissionSection />
 
-      {/* Sección Global (Imagen 3) */}
+
       <GlobalPresence />
     </div>
   );
